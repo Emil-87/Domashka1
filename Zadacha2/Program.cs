032c87514e60c4bb88dea00a476e83a113b5fbae
Emil-87 <emil.pegas@gmail.com>
2022-10-11 22:19:07 +0600

    zadacha2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,12 +1,26 @@
-﻿Console.Write("Введите первое число");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число");
-int b = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (b > a) max = b;
-Console.WriteLine(max);
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-// Задача 2.   Написать программу которая при входе имеет 2 числа, и найти мин. и макс.
-// a = 5 b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите четверть");
+int z = Convert.ToInt32(Console.ReadLine());
+if (z > 0 && z < 5)
+{
+    if (z == 1)
+    {
+        Console.WriteLine("Диапазон X 1-4 Диапазон Y 1-4");
+    }
+    if (z == 2)
+    {
+        Console.WriteLine("Диапазон X 1-4 Диапазон Y -4 - -1");
+    }
+    if (z == 3)
+    {
+        Console.WriteLine("Диапазон X -4 - -1 Диапазон Y -4 - -1");
+    }
+    if (z == 4)
+    {
+        Console.WriteLine("Диапазон X 1-4 Диапазон Y -4 - -1");
+    }
+}
+else {
+    Console.WriteLine("Ошибка диапазона");
+}
